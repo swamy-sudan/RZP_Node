8d7352172fb5fa8c8931da7b47093d4b9a4e65c5
Ayush Vipul <ayush.vipul@razorpay.com>
2021-06-09 22:45:33 +0530

    signature validate added

--- server.js
+++ server.js
@@ -42,6 +42,22 @@ app.post('/create/orderId', (req, res) => {
 })
 
 
+app.post("/api/payment/verify",(req,res)=>{
+
+ let body=req.body.response.razorpay_order_id + "|" + req.body.response.razorpay_payment_id;
+
+  var crypto = require("crypto");
+  var expectedSignature = crypto.createHmac('sha256', 'uTYMtv9W0t6RalA0WTvO4WaE')
+                                  .update(body.toString())
+                                  .digest('hex');
+                                  console.log("sig recieved " ,req.body.response.razorpay_signature);
+                                  console.log("sig generated " ,expectedSignature);
+  var response = {"signatureIsValid":"false"}
+  if(expectedSignature === req.body.response.razorpay_signature)
+   response={"signatureIsValid":"true"}
+      res.send(response);
+  });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
